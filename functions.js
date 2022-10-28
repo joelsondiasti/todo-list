72f1d5bddab7808ebe6edc80287633b21187add0
Joelson Dias <joelsondiasti@outlook.com>
2022-10-28 15:24:48 -0300

    commit inicial

---
+++ functions.js
@@ -0,0 +1,15 @@
+var form = document.getElementById("taskForm");
+var taskList = document.getElementById("taskList");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  var submit = Object.fromEntries(data);
+
+  var newTask = document.createElement("span");
+  newTask.classList.add("taskItem")
+  newTask.append(submit.task);
+
+  taskList.appendChild(newTask)
+});
+
